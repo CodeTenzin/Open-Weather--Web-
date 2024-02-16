060067cbe9ee263af3c82ca01c4ea953cc4f5745
Tenzin Wangyal <tenz_wang333@yahoo.com>
2024-02-16 13:48:03 -0500

    Removed the inner html from js.

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
 // `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=ceb16806249418e1704b7dca064ec279`
 
 let cityUserInput = document.querySelector(".container .search-box input");
-let weatherInfoContainer = document.querySelector(
-  ".container .weather-primary"
-);
-let weatherImage = document.querySelector(
-  ".container .weather-primary__img img"
-);
+// primary info
+let weatherImage = document.querySelector(".weather-primary__img img");
 let weatherTemperature = document.querySelector(
-  ".container .weather-primary__container .weather-primary__temp"
+  ".weather-primary__container .weather-primary__temp"
 );
 let weatherMain = document.querySelector(
-  ".container .weather-primary__container .weather-primary__weather"
+  ".weather-primary__container .weather-primary__weather"
 );
 let weatherDescription = document.querySelector(
-  ".container .weather-primary__container .weather-primary__description"
+  ".weather-primary__container .weather-primary__description"
 );
 let weatherLocation = document.querySelector(
-  ".container .weather-primary .weather-primary__location"
+  ".weather-primary .weather-primary__location"
 );
 // secondary info
-let otherWeatherInfo = document.querySelector(".container .weather-secondary");
+let feelsLike = document.querySelector(".feels-like");
+let minTemp = document.querySelector(".min-temp");
+let humidity = document.querySelector(".humidity");
+let windSpeed = document.querySelector(".wind-speed");
+let maxTemp = document.querySelector(".max-temp");
+let pressure = document.querySelector(".pressure");
 
 const apiKey = "ceb16806249418e1704b7dca064ec279";
 
@@ -30,28 +31,22 @@ cityUserInput.addEventListener("keyup", (e) => {
   }
 });
 
-// `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=ceb16806249418e1704b7dca064ec279`
-
 let getWeatherInfo = (cityName) => {
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`;
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
+      weatherTemperature.textContent = `${data.main.temp}\u00B0`;
       weatherImage.src = `https://api.openweathermap.org/img/w/${data.weather[0].icon}.png`;
-      weatherMain.innerHTML = data.weather[0].main;
-      weatherDescription.innerHTML = data.weather[0].description;
-      weatherLocation.innerHTML = `<i class="fa-solid fa-location-dot"></i> ${data.name}`;
-      weatherTemperature.innerHTML = `${data.main.temp}&#176;`;
-      otherWeatherInfo.innerHTML = `<div class="wind-speed"><span>Feels like</span><p>${data.main.feels_like}&#176;</p></div>
-      <div class="min-temp"><span>Min Temp</span><p>${data.main.temp_min}&#176;</p></div>
-      
-      <div class="humidity"><span>Humidity</span><p>${data.main.humidity}%</p></div>
-      
-      <div class="wind-speed"><span>Wind Speed</span><p>${data.wind.speed}mph</p></div>
-      
-      <div class="max-temp"><span>Max Temp</span><p>${data.main.temp_max}&#176;</p></div>
-      
-      <div class="pressure"><span>Pressure</span><p>${data.main.pressure}mbar</p></div>`;
+      weatherMain.textContent = data.weather[0].main;
+      weatherDescription.textContent = data.weather[0].description;
+      weatherLocation.textContent = `${data.name}`;
+      feelsLike.textContent = `${data.main.feels_like}\u00B0`;
+      minTemp.textContent = `${data.main.temp_min}\u00B0`;
+      humidity.textContent = `${data.main.humidity}%`;
+      windSpeed.textContent = `${data.wind.speed}mph`;
+      maxTemp.textContent = `${data.main.temp_max}\u00B0`;
+      pressure.textContent = `${data.main.pressure}mb`;
     });
 };
 
